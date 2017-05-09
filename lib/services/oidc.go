f12a9d17e2ccdc2989ce8dfb8edfc75c67e18882
Russell Jones <russell.valerius.jones@gmail.com>
2017-05-09 11:25:28 -0700

    Return error message for OIDC.

--- lib/services/oidc.go
+++ lib/services/oidc.go
@@ -456,7 +456,7 @@ func (o *OIDCConnectorV2) RoleFromTemplate(claims jose.Claims) (Role, error) {
 		}
 	}
 
-	return nil, trace.Errorf("unable to create role from template")
+	return nil, trace.Errorf("no matching mapping, claims: %v, claims_to_roles: %v", claims, o.Spec.ClaimsToRoles)
 }
 
 // Check returns nil if all parameters are great, err otherwise
